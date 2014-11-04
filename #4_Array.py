62c80d1cf6a6297b7fc24ef489ce461046dfbfb1
eneskemalergin <eneskemalergin@gmail.com>
2014-11-04 17:52:11 -0600

    Questions are typed...

---
+++ #4_Array.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+# Chapter 4 #
+#-----------#
+
+# Question 1#
+#------------------------------------------------------------------------------#
+'''
+Write an algorithm to calculate the sample variance of a one-dimensional
+array of numbers where the sample variance for an array containing
+N items is defined by this equation:
+
+S = 1/N (SumAll-from i=0 to N-1 with (xi - -x)^2)
+
+Here −x is the mean (average) of the values in the array, and the summation
+symbol Σ means to add up all the xi values as i varies from 0 to N – 1.
+'''
+
+
+
+
+#------------------------------------------------------------------------------#
+# Question 2
+
+'''
+Write an algorithm to calculate the sample standard deviation of a onedimensional
+array of numbers where the sample standard deviation is
+defi ned to be the square root of the sample variance.
+'''
+
+
+
+#------------------------------------------------------------------------------#
+
+# Question 3
+
+'''
+Write an algorithm to fi nd the median of a sorted one-dimensional array.
+(Be sure to handle arrays holding an even or odd number of items.)
+'''
+
+
+
+
+#------------------------------------------------------------------------------#
+
+# Question 4
+
+'''
+The section “Removing Items” explained how to remove an item from a
+linear array. Write the algorithm in pseudocode.
+'''
+
+#------------------------------------------------------------------------------#
+
+# Question 5
+
+'''
+The triangular arrays discussed in this chapter are sometimes called “lower
+triangular arrays” because the values are stored in the lower-left half of
+the array. How would you modify that kind of array to produce an upper
+triangular array with the values stored in the upper-right corner?
+'''
+
+#------------------------------------------------------------------------------#
+
+# Question 6
+
+'''
+How would you modify the lower triangular arrays described in this
+chapter to make an “upper-left” array where the entries are stored in the
+upper-left half of the array? What is the relationship between row and
+column for the entries in the array?
+'''
+
+
+
+
+#------------------------------------------------------------------------------#
+# Question 7
+
+'''
+Suppose you defi ne the main diagonal of a rectangular (and nonsquare)
+array to start in the upper-left corner and extend down and to the right
+until it reaches the bottom or right edge of the array. Write an algorithm
+that fi lls entries on or below the main diagonal with 1s and entries above
+the main diagonal with 0s.
+'''
+
+
+
+
+#------------------------------------------------------------------------------#
+
+
+# Question 8
+
+'''
+Consider the diagonal of a rectangular array that starts in the last column
+of the fi rst row and extends left and down until it reaches the bottom or
+left edge of the array. Write an algorithm that fi lls the entries on or above
+the diagonal with 1s and entries below the diagonal with 0s.
+'''
+
+
+
+
+
+#------------------------------------------------------------------------------#
+
+# Question 9
+
+'''
+Write an algorithm that fi lls each item in a rectangular array with the
+distance from that entry to the nearest edge of the array.
+'''
+
+
+
+
+#------------------------------------------------------------------------------#
+
+#Question 10
+
+'''
+*Generalize the method for building triangular arrays to build threedimensional
+tetrahedral arrays that contain entries value[i, j, k] where
+j ≤ i and k ≤ j. How would you continue to extend this method for even
+higher dimensions?
+'''
+
+
+#------------------------------------------------------------------------------#
+
+#Question 11
+
+'''
+How could you make a sparse triangular array?
+'''
+
+#------------------------------------------------------------------------------#
+
+# Question 12
+
+'''
+ Write an algorithm that adds two triangular arrays.
+
+'''
+
+
+#------------------------------------------------------------------------------#
+
+# Question 13
+
+'''
+ Write an algorithm that multiplies two triangular arrays.
+
+'''
+
+
+
+#------------------------------------------------------------------------------#
+
+
+# Question 14
+
+'''
+The algorithm described for adding two sparse matrices is fairly highlevel.
+Expand the algorithm to provide details in place of the instructions
+inside the angle brackets (<>). (Hint: You may want to make a separate
+CopyEntries method to copy entries from one list to another and a separate
+AddEntries method to combine the entries in two rows with the same row
+number.)
+'''
+
+#------------------------------------------------------------------------------#
+
+
+# Question 15
+
+'''
+At a high level, write an algorithm that effi ciently multiplies two sparse
+matrices that have default value 0.
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
