236a91bb1b5bc1d6354e1e70516bf751f8e8a1ba
eneskemalergin <eneskemalergin@gmail.com>
2014-11-12 20:57:34 -0600

    Stack and Queue exercise questions...

---
+++ #5_Stacks_Queues.py
@@ -0,0 +1,153 @@
+# -*- coding: cp1252 -*-
+# Chapter 5 #
+#-----------#
+# Exercises #
+
+"""##############################################################################"""
+# Question 1:
+
+''' When you use a double stack, what is the relationship between the variables
+        NextIndex1 and NextIndex2 when one of the stacks is full?
+'''
+
+"""When one of the stacks is full, NextIndex1 > NextIndex2. At that point both
+stacks are full, NextIndex1 is the index of the top item in the second stack, and
+NextIndex2 is the index of the top item in the fi rst stack."""
+
+
+
+#--------------------------------------------------------------------------------#
+
+# Question 2:
+
+''' Write an algorithm that takes as input a stack and returns a new stack
+        containing the same items but in reverse order.
+'''
+def ReverseStack(value):
+    Stack.new_stack
+    while(value != None):
+        new_stack.Push(value.Pop())
+
+#--------------------------------------------------------------------------------#
+
+# Question 3:
+
+''' Write a program that implements insertionsort with stacks. '''
+
+
+
+
+
+#--------------------------------------------------------------------------------#
+
+# Question 4:
+
+''' For each item, the stack insertionsort algorithm moves the unsorted items
+to the temporary stack. Then it moves some of the sorted items to the
+temporary stack, and then it moves all the items back to the original stack.
+Does it really need to move all the items back to the original stack? Can
+you improve the algorithm’s performance by modifying that step? What
+does that do to the algorithm’s Big O run time?
+
+'''
+
+
+
+
+
+#--------------------------------------------------------------------------------#
+
+# Question 5:
+
+''' What does the stack insertionsort algorithm mean in terms of train sorting? '''
+
+
+
+
+#--------------------------------------------------------------------------------#
+
+# Question 6:
+
+''' Write a program that implements selectionsort with stacks. '''
+
+
+
+
+#-------------------------------------------------------------------------------#
+
+# Question 7:
+
+''' What does the stack selectionsort algorithm mean in terms of train sorting? '''
+
+
+
+
+#-------------------------------------------------------------------------------#
+
+# Question 8:
+
+''' Write a program that implements a priority queue. '''
+
+
+
+
+#--------------------------------------------------------------------------------#
+
+# Question 9:
+
+''' Write a program that implements a deque. '''
+
+
+
+
+
+#--------------------------------------------------------------------------------#
+
+# Question 11:
+
+'''Write a program that implements insertionsort with queues.'''
+
+
+
+
+
+
+#--------------------------------------------------------------------------------#
+
+# Question 12:
+
+''' Write a program that implements selectionsort with queues.'''
+
+
+
+
+
+
+#--------------------------------------------------------------------------------#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
