4a3342231aa9ff053bde49d943d1c3dedadadfd8
eneskemalergin <eneskemalergin@gmail.com>
2014-11-26 18:02:24 -0600

    Quick Sort with O(NlogN)

---
+++ QuickSort_algorithm.py
@@ -0,0 +1,23 @@
+''' Quick Sort   '''
+def quickSort(arr):
+    less = []
+    pivotList = []
+    more = []
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        for i in arr:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                more.append(i)
+            else:
+                pivotList.append(i)
+        less = quickSort(less)
+        more = quickSort(more)
+        return less + pivotList + more
+
+# quickSort([4, 65, 2, -31, 0, 99, 83, 782, 1])
+#[-31, 0, 1, 2, 4, 65, 83, 99, 782]
+
