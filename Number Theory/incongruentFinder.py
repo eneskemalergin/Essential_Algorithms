a277923e6890286f32ceead35571cd57f15608a1
Enes Kemal Ergin <eneskemalergin@gmail.com>
2015-04-01 11:56:13 -0500

    Bug fixed Algorithm is working

--- Number Theory/incongruentFinder.py
+++ Number Theory/incongruentFinder.py
@@ -1,4 +1,4 @@
-# Finding all incongruences
+# Finding all incongruences for linear equations
 
 def extendedGCD(a, b):
     if a == 0:
@@ -13,23 +13,30 @@ a  = 6
 mod = 514
 result = 15
 """
-# There is a bug to fix
+
 def incongrFind(a, mod, result):
 	func = extendedGCD(a, mod)
 	g = func[0]
 	x = func[1]
 	y = func[2]
 	list_ = []
-	if (result/g) != int:
-		return list_
-	if g != result:
-		x0 = result / g
 	
+	#if gcd(a, mod) does not divide result
+	if result % g != 0:
+		# There is no solution 
+		return list_
+	
+	x0 = x * (result/g)
 	while g > 0:
 		list_.append(x0)
-		x0 = x0 + g * (mod/g)
+		x0 = x0 + (g * ( mod / g))
 		g -= 1
 
 	return list_
+
+# Test Cases	
 print(incongrFind(42, 78, 12))
-print(incongrFind(943, 2576, 381)) # Supposed to be No solutions
+print(incongrFind(943, 2576, 381)) 
+print(incongrFind(51, 1008, 177))
+print(incongrFind(97, 53460, 1))
+print(incongrFind(4, 19, 3))
